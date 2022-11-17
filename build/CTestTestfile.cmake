1ea1bf6c7d2cd8ebd3a5377375ce4fd0f337f95b
HwangSeokHwan98 <pop0479@hanyang.ac.kr>
2022-11-17 15:20:55 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,22 @@
+# CMake generated Testfile for 
+# Source directory: /home/hsh/catkin_ws/src
+# Build directory: /home/hsh/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mobile_robot")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3_msgs")
+subdirs("turtlebot3/turtlebot3_navigation")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("DynamixelSDK/ros/dynamixel_sdk")
+subdirs("DynamixelSDK/ros/dynamixel_sdk_examples")
+subdirs("teleop_twist_keyboard_cpp")
+subdirs("turtlebot3/turtlebot3_bringup")
+subdirs("turtlebot3/turtlebot3_example")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
+subdirs("turtlebot3/turtlebot3_slam")
+subdirs("turtlebot3/turtlebot3_teleop")
+subdirs("turtlebot3/turtlebot3_description")
